8fc43ad19e4bf03639755615016ad079f6b5ed7d
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-02 15:41:34 +0700

    http://www.usaco.org/index.php?page=viewproblem2&cpid=786 solved

--- LIFEGUARDS/lifeguards.cpp
+++ LIFEGUARDS/lifeguards.cpp
@@ -7,12 +7,13 @@
 #define maxn int(1e9)
 #define cut cout << endl
 #define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
-#define Fin(name) freopen(name ".inp", "r", stdin)
+#define Fin(name) freopen(name ".in", "r", stdin)
 #define Fout(name) freopen(name ".out", "w", stdout)
 #define forup(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i <= i##_end; ++i)
 #define fordown(type, i, start, end) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i >= i##_end; --i)
 #define allVi(x) x.begin(), x.end()
 #define allArr(x, start, end) x, x + begin, x + end + begin
+#define pb push_back
 
 typedef long long ll;
 typedef unsigned long long ull;
@@ -30,29 +31,29 @@ int main()
 #ifndef ONLINE_JUDGE
     IO();
 #endif
-    int n;
+    int n(0);
     cin >> n;
-
     vector<pair<int, int>> a;
-    a.push_back({0, 0});
-    for (int i = 0; i < n; i++)
+    a.pb({0, 0});
+    a.pb({maxn, maxn});
+    forup(int, i, 1, n)
     {
-        int l, r;
-        cin >> l >> r;
-        a.push_back({l, r});
+        int u(0), v(0);
+        cin >> u >> v;
+        a.pb({u, v});
     }
-    a.push_back({maxn, maxn});
     sort(allVi(a));
 
-    int tot = 0, mn = maxn, l_end = -1, l_far = -1, r_end = -1;
-    for (int i = 1; i <= n; i++)
+    int l(-1), r(-1), highest_l(-1), minimalLost(maxn), totalTime(0);
+    forup(int, i, 1, n)
     {
-        l_end = max(a[i].first, a[i - 1].second);
-        l_far = max(l_far, l_end);
-        r_end = min(a[i].second, a[i + 1].first);
-        mn = min(mn, max(0, r_end - l_end));
-        tot += max(0, a[i].second - l_far);
+        l = max(a[i].first, a[i - 1].second);
+        highest_l = max(highest_l, l);
+        r = min(a[i].second, a[i + 1].first);
+        minimalLost = min(minimalLost, max(0, r - l));
+        totalTime += max(0, a[i].second - highest_l);
     }
-    cout << tot - mn;
+    cout << totalTime - minimalLost;
+
     return 0;
 }
