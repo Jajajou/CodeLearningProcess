b7df608bfef585b2209f0bcf4ceb938ef9c544d1
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-02 15:19:01 +0700

    http://www.usaco.org/index.php?page=viewproblem2&cpid=786 solved

--- LIFEGUARDS/lifeguards.cpp
+++ LIFEGUARDS/lifeguards.cpp
@@ -4,7 +4,7 @@
 */
 #include <bits/stdc++.h>
 #define name "lifeguards" //pls dont forget your task's name
-#define maxn 101001
+#define maxn int(1e9)
 #define cut cout << endl
 #define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define Fin(name) freopen(name ".inp", "r", stdin)
@@ -16,7 +16,6 @@
 
 typedef long long ll;
 typedef unsigned long long ull;
-typedef pair<int, int> pii;
 
 const void IO()
 {
@@ -31,7 +30,29 @@ int main()
 #ifndef ONLINE_JUDGE
     IO();
 #endif
-    int n(0);
+    int n;
     cin >> n;
+
+    vector<pair<int, int>> a;
+    a.push_back({0, 0});
+    for (int i = 0; i < n; i++)
+    {
+        int l, r;
+        cin >> l >> r;
+        a.push_back({l, r});
+    }
+    a.push_back({maxn, maxn});
+    sort(allVi(a));
+
+    int tot = 0, mn = maxn, l_end = -1, l_far = -1, r_end = -1;
+    for (int i = 1; i <= n; i++)
+    {
+        l_end = max(a[i].first, a[i - 1].second);
+        l_far = max(l_far, l_end);
+        r_end = min(a[i].second, a[i + 1].first);
+        mn = min(mn, max(0, r_end - l_end));
+        tot += max(0, a[i].second - l_far);
+    }
+    cout << tot - mn;
     return 0;
 }
