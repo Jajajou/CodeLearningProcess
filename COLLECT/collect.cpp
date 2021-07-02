774c7a55e634cb0fa81a9da4a35c9fbe87028d79
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-02 17:01:43 +0700

    https://oj.vnoi.info/problem/collect solving

---
+++ COLLECT/collect.cpp
@@ -0,0 +1,52 @@
+/*   __               (\_/)
+  ._( .)<(MEOW) (WOOF)('V')
+   \____)             (U U)
+*/
+#include <bits/stdc++.h>
+#define name "collect" //pls dont forget your task's name
+#define maxn 101001
+#define cut cout << endl
+#define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define Fin(name) freopen(name ".inp", "r", stdin)
+#define Fout(name) freopen(name ".out", "w", stdout)
+#define forup(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i <= i##_end; ++i)
+#define fordown(type, i, start, end) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i >= i##_end; --i)
+#define allVi(x) x.begin(), x.end()
+#define allArr(x, start, end) x, x + begin, x + end + begin
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+const void IO()
+{
+    Fin(name);
+    Fout(name);
+}
+using namespace std;
+struct collection
+{
+    int z, s, m, k;
+    collection(int z, int s, int m, int k) : z(z), s(s), m(m), k(k) {}
+    collection(int z, int s, int m) : z(z), s(s), m(m) {}
+};
+vector<pair<collection, collection>> trade;
+
+void read()
+{
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    boost();
+#ifndef ONLINE_JUDGE
+    IO();
+#endif
+    read();
+    solve();
+    return 0;
+}
