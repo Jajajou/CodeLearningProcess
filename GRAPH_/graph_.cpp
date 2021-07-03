895a48009796bc92d82cfb559a50c70e48f3b442
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-03 15:14:53 +0700

    https://oj.vnoi.info/problem/graph_ solved

--- GRAPH_/graph_.cpp
+++ GRAPH_/graph_.cpp
@@ -4,7 +4,7 @@
 */
 #include <bits/stdc++.h>
 #define name "graph_" //pls dont forget your task's name
-#define maxn 101001
+#define maxn int(1e4) + 14
 #define cut cout << endl
 #define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define Fin(name) freopen(name ".inp", "r", stdin)
@@ -24,13 +24,52 @@ const void IO()
     Fout(name);
 }
 using namespace std;
+int n(0), m(0), criticalNode(0), criticalEdge(0);
+vector<int> G[maxn];
 
 void read()
 {
+    cin >> n >> m;
+    int u(0), v(0);
+    while (cin >> u >> v)
+    {
+        G[u].pb(v);
+        G[v].pb(u);
+    }
+}
+
+int low[maxn] = {}, num[maxn] = {}, checkedNode[maxn] = {}, cnt(0);
+void visit(int u, int cp)
+{
+    int childNode(0);
+    low[u] = num[u] = ++cnt;
+    for (int v : G[u])
+    {
+        if (v - cp)
+        {
+            if (!num[v])
+            {
+                visit(v, u);
+                ++childNode;
+                low[u] = min(low[u], low[v]);
+                criticalEdge += (low[v] == num[v]);
+                if (u == cp and childNode >= 2 || u - cp and low[v] >= num[u])
+                {
+                    if (!checkedNode[u])
+                        checkedNode[u] = ++criticalNode;
+                }
+            }
+            else
+                low[u] = min(low[u], num[v]);
+        }
+    }
 }
 
 void solve()
 {
+    forup(int, u, 1, n) if (!num[u]) visit(u, u);
+    // forup(int, u, 1, n) cout << u << ' ' << num[u] << ' ' << low[u] << endl;
+    cout << criticalNode << ' ' << criticalEdge;
 }
 
 int main()
