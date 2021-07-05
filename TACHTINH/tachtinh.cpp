e80d236ed91711becc22e7ba516f8326eb55fb4c
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-06 00:15:58 +0700

    http://hoc.chuyentin.pro:8003/contests/test/TACHTINH/TACHTINH.png solved

--- TACHTINH/tachtinh.cpp
+++ TACHTINH/tachtinh.cpp
@@ -4,7 +4,7 @@
 */
 #include <bits/stdc++.h>
 #define name "tachtinh" //pls dont forget your task's name
-#define maxn 101001
+#define maxn int(1e5)
 #define pri_q priority_queue
 #define pf push_front
 #define pb push_back
@@ -30,13 +30,43 @@ const void IO()
     Fout(name);
 }
 using namespace std;
+int n(0), a[maxn] = {}, sum(0);
+vector<int> G[maxn];
 
 void read()
 {
+    cin >> n;
+    forup(int, i, 1, n) cin >> a[i], sum += a[i];
+    int u(0), v(0);
+    while (cin >> u >> v)
+    {
+        G[u].pb(v);
+        G[v].pb(u);
+    }
+}
+
+int depth[maxn] = {};
+bool visited[maxn] = {};
+void dfs(int u)
+{
+    depth[u] = a[u];
+    visited[u] = 1;
+    for (int v : G[u])
+    {
+        if (!visited[v])
+        {
+            dfs(v);
+            depth[u] += depth[v];
+        }
+    }
 }
 
 void solve()
 {
+    dfs(1);
+    int res(INT_MAX);
+    forup(int, i, 1, n) res = min(res, abs(sum - 2 * depth[i]));
+    cout << res;
 }
 
 int main()
