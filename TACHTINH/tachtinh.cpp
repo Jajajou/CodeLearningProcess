e7b78765151c79cae6085b7b00798bcbb6909115
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-05 23:36:52 +0700

    http://hoc.chuyentin.pro:8003/contests/test/TACHTINH/TACHTINH.png solving

---
+++ TACHTINH/tachtinh.cpp
@@ -0,0 +1,51 @@
+/*   __               (\_/)
+  ._( .)<(MEOW) (WOOF)('V')
+   \____)             (U U)
+*/
+#include <bits/stdc++.h>
+#define name "tachtinh" //pls dont forget your task's name
+#define maxn 101001
+#define pri_q priority_queue
+#define pf push_front
+#define pb push_back
+#define popb pop_back
+#define popf pop_front
+#define fi first
+#define se second
+#define cut cout << endl
+#define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define Fin(name) freopen(name ".inp", "r", stdin)
+#define Fout(name) freopen(name ".out", "w", stdout)
+#define forup(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i <= i##_end; ++i)
+#define fordown(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i >= i##_end; --i)
+#define allVi(x) x.begin(), x.end()
+#define allArr(x, start, end) x, x + begin, x + end + begin
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+const void IO()
+{
+    Fin(name);
+    Fout(name);
+}
+using namespace std;
+
+void read()
+{
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    boost();
+#ifndef ONLINE_JUDGE
+    IO();
+#endif
+    read();
+    solve();
+    return 0;
+}
