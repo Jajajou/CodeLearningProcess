0c020da9c78c0e8907b11926d857aa846c3a61e1
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-05 13:27:51 +0700

    http://hoc.chuyentin.pro:8003/contests/test/CTDON260/CTDON260.png solving

---
+++ CTDON269/ctdon260.cpp
@@ -0,0 +1,45 @@
+/*   __               (\_/)
+  ._( .)<(MEOW) (WOOF)('V')
+   \____)             (U U)
+*/
+#include <bits/stdc++.h>
+#define name "ctdon260" //pls dont forget your task's name
+#define maxn 101001
+#define cut cout << endl
+#define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define Fin(name) freopen(name ".inp", "r", stdin)
+#define Fout(name) freopen(name ".out", "w", stdout)
+#define forup(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i <= i##_end; ++i)
+#define fordown(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i >= i##_end; --i)
+#define allVi(x) x.begin(), x.end()
+#define allArr(x, start, end) x, x + begin, x + end + begin
+#define pb push_back
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+const void IO()
+{
+    Fin(name);
+    Fout(name);
+}
+using namespace std;
+
+void read()
+{
+}
+
+void solve()
+{
+}
+
+int main()
+{
+    boost();
+#ifndef ONLINE_JUDGE
+    IO();
+#endif
+    read();
+    solve();
+    return 0;
+}
