e6d42e9c61fbe924529f57e0e36f21693da074a6
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-05 14:16:35 +0700

    http://hoc.chuyentin.pro:8003/contests/test/CTDON260/CTDON260.png solved

--- CTDON269/ctdon260.cpp
+++ CTDON269/ctdon260.cpp
@@ -4,7 +4,7 @@
 */
 #include <bits/stdc++.h>
 #define name "ctdon260" //pls dont forget your task's name
-#define maxn 101001
+#define maxn int(1e5) + 15
 #define cut cout << endl
 #define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 #define Fin(name) freopen(name ".inp", "r", stdin)
@@ -24,13 +24,58 @@ const void IO()
     Fout(name);
 }
 using namespace std;
+int n(0), m(0), low[maxn] = {}, num[maxn] = {}, cnt(0);
+vector<int> G[maxn], onlyTreeGraph[maxn];
 
 void read()
 {
+    cin >> n >> m;
+    int u(0), v(0);
+    while (cin >> u >> v)
+    {
+        G[u].pb(v);
+        G[v].pb(u);
+    }
+}
+
+int path[maxn] = {};
+void visit(int u)
+{
+    low[u] = num[u] = ++cnt;
+    for (int v : G[u])
+    {
+        if (!num[v])
+        {
+            path[v] = u;
+            visit(v);
+            low[u] = min(low[u], low[v]);
+        }
+        else if (path[u] != v)
+            low[u] = min(low[u], num[v]);
+    }
+}
+
+bool visited[maxn] = {};
+void dfs(int u, int &child)
+{
+    ++child, visited[u] = 1;
+    for (int v : onlyTreeGraph[u])
+        if (!visited[v])
+            dfs(v, child);
 }
 
 void solve()
 {
+    forup(int, i, 1, n) if (!num[i]) cnt = 0, visit(i);
+    forup(int, i, 2, n) if (num[i] > 1 && low[i] == num[i]) { onlyTreeGraph[path[i]].pb(i), onlyTreeGraph[i].pb(path[i]); }
+    int res(0);
+    forup(int, i, 1, n) if (!visited[i])
+    {
+        int cp(0);
+        dfs(i, cp);
+        res += cp * (cp - 1) / 2 - (cp - 1);
+    }
+    cout << res;
 }
 
 int main()
