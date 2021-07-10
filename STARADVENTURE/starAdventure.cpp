3620a7d991c19ab62ed2601918b3342fdfc6f0e8
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-10 15:46:41 +0700

    not solved

--- STARADVENTURE/starAdventure.cpp
+++ STARADVENTURE/starAdventure.cpp
@@ -33,16 +33,20 @@ using namespace std;
 
 class beatTheGame
 {
-    int data[50][50];
-    int n, m, k, levelDataSum[50][50][50], dp[50][50][50];
+    int data[50][50], curr[50][50][50], n, m, k, levelDataSum[50][50][50], dp[50][50][50];
 
     void emptyAllMatrix()
     {
         forup(int, i, 0, 49) forup(int, j, 0, 49)
         {
             data[i][j] = 0;
-            forup(int, k, i, 49) levelDataSum[i][j][k] = dp[i][j][k] = 0;
+            forup(int, k, 0, 49) levelDataSum[i][j][k] = dp[i][j][k] = 0;
         }
+    }
+
+    void resetCurr()
+    {
+        forup(int, i, 0, 49) forup(int, j, 0, 49) forup(int, k, 0, 49) curr[i][j][k] = 0;
     }
 
     void initData(string inp[])
@@ -52,6 +56,11 @@ class beatTheGame
             for (int j = 0; j < m; ++j)
                 data[i][j] = inp[i][j] - '0';
         }
+    }
+
+    void merge(int a[50][50][50], int b[50][50][50])
+    {
+        forup(int, i, 0, m - 1) forup(int, j, 0, m - 1) forup(int, k, 0, m - 1) a[i][j][k] = b[i][j][k];
     }
 
     void calDataSum()
@@ -78,15 +87,22 @@ class beatTheGame
                 forup(int, i, 0, n - 1) res += levelDataSum[i][0][m - 1];
                 return res;
             }();
-        int curr[m][m][m];
-        forup(int, lever, 0, n - 1)
+        forup(int, level, 0, n - 1)
         {
+            resetCurr();
             forup(int, i, 0, m - 3) forup(int, j, i + 1, m - 2) forup(int, k, j + 1, m - 1)
             {
-                curr[i][j][k] =
+                curr[i][j][k] = [&](int level, int endPos_1, int endPos_2, int endPos_3)
+                {
+                    int res(0);
+                    forup(int, i, 0, endPos_1) forup(int, j, endPos_1 + 1, endPos_2) forup(int, k, endPos_2 + 1, endPos_3)
+                        res = max(res, dp[i][j][k] + levelDataSum[level][i][endPos_1] + levelDataSum[level][j][endPos_2] + levelDataSum[level][j][endPos_3]);
+                    return res;
+                }(level, i, j, k);
             }
+            merge(dp, curr);
         }
-        return res;
+        return dp[m - 3][m - 2][m - 1];
     }
 
 public:
