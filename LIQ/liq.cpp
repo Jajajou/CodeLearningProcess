b45abc0fb59b3a97eb59cb3d295f172101850af7
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-10 10:08:37 +0700

    https://oj.vnoi.info/problem/liq solved

---
+++ LIQ/liq.cpp
@@ -0,0 +1,52 @@
+/*   __               (\_/)
+  ._( .)<(MEOW) (WOOF)('V')
+   \____)             (U U)
+*/
+#include <bits/stdc++.h>
+#define name "liq" //pls dont forget your task's name
+#define maxn 101001
+#define pri_q priority_queue
+#define pf push_front
+#define pb push_back
+#define popb pop_back
+#define popf pop_front
+#define fi first
+#define se second
+#define cut cout << endl
+#define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define Fin(name) freopen(name ".inp", "r", stdin)
+#define Fout(name) freopen(name ".out", "w", stdout)
+#define forup(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i <= i##_end; ++i)
+#define fordown(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i >= i##_end; --i)
+#define allVi(x) x.begin(), x.end()
+#define allArr(x, start, end) x, x + begin, x + end + begin
+
+typedef long long ll;
+typedef unsigned long long ull;
+
+const void IO()
+{
+    Fin(name);
+    Fout(name);
+}
+using namespace std;
+
+int main()
+{
+    boost();
+#ifndef ONLINE_JUDGE
+    IO();
+#endif
+    int n(0);
+    cin >> n;
+
+    int dp[n + 1] = {}, arr[n + 1] = {}, res(INT_MIN);
+    forup(int, i, 1, n)
+    {
+        cin >> arr[i];
+        forup(int, j, 0, i)
+            res = max(res, dp[i] = (arr[j] < arr[i]) ? max(dp[i], dp[j] + 1) : dp[i]);
+    }
+    cout << res;
+    return 0;
+}
