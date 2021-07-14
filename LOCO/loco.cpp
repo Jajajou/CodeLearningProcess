9783d79a67c1a7ba60d798d335de5a8f62da604a
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-15 00:22:52 +0700

    http://hoc.chuyentin.pro:8003/contests/test/LOCO/LOCO.png solved

--- LOCO/loco.cpp
+++ LOCO/loco.cpp
@@ -4,7 +4,6 @@
 */
 #include <bits/stdc++.h>
 #define name "loco" //pls dont forget your task's name
-#define maxn 101001
 #define pri_q priority_queue
 #define pf push_front
 #define pb push_back
@@ -23,6 +22,15 @@
 
 typedef long long ll;
 typedef unsigned long long ull;
+template <class val>
+val mul(val a, val b, val m)
+{
+    if (!b)
+        return 0;
+    if (!(b - 1))
+        return a;
+    return b & 1 ? (mul(a, b >> 1, m) * 2 + a) % m : (mul(a, b >> 1, m) * 2) % m;
+}
 
 const void IO()
 {
@@ -30,14 +38,57 @@ const void IO()
     Fout(name);
 }
 using namespace std;
+ll n(0), maxn(0);
 
-void read()
+class matrix
 {
-}
+public:
+    ll res[3][3];
 
-void solve()
-{
-}
+    matrix()
+    {
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                res[i][j] = 0LL;
+        for (int i = 0; i < 3; ++i)
+            res[0][i] = 1LL;
+        res[1][0] = res[2][1] = 1LL;
+    }
+
+    matrix(ll tmp[][3]) { forup(int, i, 0, 2) forup(int, j, 0, 2) res[i][j] = tmp[i][j]; }
+
+    matrix operator%(const ll &k)
+    {
+        ll tmp[3][3];
+        for (int i = 0; i < 3; ++i)
+            for (int j = 0; j < 3; ++j)
+                tmp[i][j] = res[i][j] % k;
+        return matrix(tmp);
+    }
+
+    matrix operator*(const matrix &m)
+    {
+        ll tmp[3][3];
+        forup(int, i, 0, 2) forup(int, j, 0, 2)
+        {
+            tmp[i][j] = [&]()
+            {
+                ll cp(0);
+                forup(int, k, 0, 2) cp = ((cp % maxn) + (mul(res[i][k] % maxn, m.res[k][j] % maxn, maxn) % maxn)) % maxn;
+                return cp;
+            }();
+        }
+        return matrix(tmp) % maxn;
+    }
+
+    matrix operator^=(const ll &n)
+    {
+        if (n <= 1)
+            return matrix();
+        matrix res = *this ^= (n >> 1);
+        return n & 1 ? res * res * matrix() : res * res;
+    }
+};
 
 int main()
 {
@@ -45,7 +96,8 @@ int main()
 #ifndef ONLINE_JUDGE
     IO();
 #endif
-    read();
-    solve();
+    cin >> n >> maxn;
+    matrix ans = matrix() ^= n;
+    cout << ans.res[0][0];
     return 0;
 }
