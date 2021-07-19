e5b98d95457f674846f0c327c721f0948f35980d
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-20 01:23:26 +0700

    http://hoc.chuyentin.pro:8003/contests/test/SELECT/SELECT.png solving

--- SELECT/select.cpp
+++ SELECT/select.cpp
@@ -4,7 +4,7 @@
 */
 #include <bits/stdc++.h>
 #define name "select" //pls dont forget your task's name
-#define maxn 20
+#define maxn 101001
 #define pri_q priority_queue
 #define pf push_front
 #define pb push_back
@@ -22,7 +22,7 @@
 #define allArr(x, start, end) x, x + begin, x + end + begin
 
 template <class val>
-val getBit(val x, val pos) { return x >> pos & 1; }
+val getBit(val x, val pos) { return x >> (pos - 1) & 1; }
 template <class val>
 val setBitVal(val pos, val x, val &inp) { return (x == 1) ? inp |= (1 << (pos - 1)) : inp &= ~(1 << (pos - 1)); }
 
@@ -35,6 +35,36 @@ const void IO()
     Fout(name);
 }
 using namespace std;
+int n(0);
+ll **a(NULL);
+ll *dp(NULL);
+
+void read()
+{
+    cin >> n;
+    a = new ll *[n];
+    for (int i = 0; i < n; ++i)
+    {
+        a[i] = new ll[n];
+        for (int j = 0; j < n; ++j)
+            cin >> a[i][j];
+    }
+    dp = new ll[1 << n];
+    for (int state(0); state < 1 << n; ++state)
+        dp[state] = 0;
+}
+
+void solve()
+{
+    for (int state = 1; state < (1 << n); ++state)
+    {
+        int row(__builtin_popcount(state) - 1);
+        for (int i = 0; i < n; ++i)
+            if (getBit(state, i + 1))
+                dp[state] = max(dp[state], dp[state ^ (1 << i)] + a[row][i]);
+    }
+    cout << dp[(1 << n) - 1];
+}
 
 int main()
 {
@@ -42,23 +72,11 @@ int main()
 #ifndef ONLINE_JUDGE
     IO();
 #endif
-    int n(0);
-    cin >> n;
-
-    int a[n][n], dp[1 << n];
+    read();
+    solve();
     for (int i = 0; i < n; ++i)
-        for (int j = 0; j < n; ++j)
-            cin >> a[i][j];
-    for (int state = 0; state < 1 << n; ++state)
-        dp[state] = 0;
-
-    for (int state = 1; state < 1 << n; ++state)
-    {
-        int row(__builtin_popcount(state) - 1);
-        for (int i = 0; i < n; ++i)
-            if (getBit(state, i))
-                dp[state] = max(dp[state], dp[state ^ (1 << i)] + a[row][i]);
-    }
-    cout << dp[(1 << n) - 1];
+        delete[] a[i];
+    delete[] a;
+    delete[] dp;
     return 0;
 }
