302e881cab45466cd0f6efca68ac94bfb2b2caa8
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-07-30 07:28:38 +0700

    http://hoc.chuyentin.pro:8003/contests/test/RGAME/RGAME.png sovling

---
+++ RGAME/rgame.cpp
@@ -0,0 +1,81 @@
+/*   __               (\_/)
+  ._( .)<(MEOW) (WOOF)('V')
+   \____)             (U U)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define name "" //pls dont forget your task's name
+#define maxn 101001
+#define pri_q priority_queue
+#define pf push_front
+#define pb push_back
+#define popb pop_back
+#define popf pop_front
+#define fi first
+#define se second
+#define cut cout << endl
+#define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define Fin(name) freopen(name ".inp", "r", stdin)
+#define Fout(name) freopen(name ".out", "w", stdout)
+#define forup(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i <= i##_end; ++i)
+#define fordown(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i >= i##_end; --i)
+#define allVi(x) x.begin(), x.end()
+#define allArr(x, start, end) x, x + begin, x + end + begin
+
+template <class val>
+val getBit(val x, val pos)
+{
+    return x >> pos & 1;
+}
+template <class val>
+val setBitVal(val pos, val x, val &inp) { return (x == 1) ? inp |= (1 << pos) : inp &= ~(1 << pos); }
+
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+
+const void IO()
+{
+    Fin(name);
+    Fout(name);
+}
+
+void solve()
+{
+    int n(0);
+    cin >> n;
+    vector<int> arr(n, 0);
+    --n;
+    forup(int, i, 0, n)
+            cin >>
+        arr[i];
+    sort(arr.begin(), arr.end(), greater<int>());
+    int res(1), cp(2);
+    forup(int, i, 0, n - 1)
+    {
+        if (arr[i] == arr[i + 1])
+        {
+            i += 2, --cp;
+            res *= arr[i];
+        }
+        if (!cp || i > n)
+            break;
+    }
+
+    return (void)(cout << (cp == 0 ? res : -1) << endl);
+}
+
+int main()
+{
+    boost();
+#ifndef ONLINE_JUDGE
+    IO();
+#endif
+    int t(0);
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
