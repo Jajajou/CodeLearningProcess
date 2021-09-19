e70054ed53908b58e0f48b690ac61159a194a9b4
Đặng Trần Long <dangtranlong2015@gmail.com>
2021-09-19 14:56:44 +0700

    solve

---
+++ CSES_ADDITION/BIT_PROBLEM/bp.cpp
@@ -0,0 +1,74 @@
+/*   __               (\_/)
+  ._( .)<(MEOW) (WOOF)('V')
+   \____)             (U U)
+*/
+#include <bits/stdc++.h>
+using namespace std;
+#define name "bp" //pls dont forget your task's name
+#define maxn 101001
+#define elif else if
+#define pri_q priority_queue
+#define pf push_front
+#define pb push_back
+#define popb pop_back
+#define popf pop_front
+#define fi first
+#define se second
+#define cut cout << endl
+#define boost() ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
+#define Fin(name) freopen(name ".inp", "r", stdin)
+#define Fout(name) freopen(name ".out", "w", stdout)
+#define forup(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i <= i##_end; ++i)
+#define fordown(type, i, start, stop) for (type i = (type)(start), i##_end = static_cast<decltype(i)>(stop); i >= i##_end; --i)
+#define allVi(x) x.begin(), x.end()
+#define allArr(x, start, end) x, x + begin, x + end + begin
+
+template <class val>
+val getBit(val x, val pos)
+{
+    return x >> pos & 1;
+}
+template <class val>
+val setBitVal(val pos, val x, val &inp) { return (x == 1) ? inp |= (1 << pos) : inp &= ~(1 << pos); }
+template <class val>
+const void maximize(val &a, val b)
+{
+    a = max(a, b);
+}
+template <class val>
+const void minimize(val &a, val b)
+{
+    a = min(a, b);
+}
+typedef long long ll;
+typedef unsigned long long ull;
+typedef pair<int, int> ii;
+typedef tuple<int, int, int> iii;
+typedef const void (*funcc)(int &, int);
+
+const void IO()
+{
+    Fin(name);
+    Fout(name);
+}
+
+int main()
+{
+    boost();
+#ifndef ONLINE_JUDGE
+    IO();
+#endif
+    int n(0);
+    cin >> n;
+    vector<int> arr(n), dp1(1 << 20, 0), dp2(1 << 20, 0);
+    for (int &v : arr)
+        cin >> v, ++dp1[v], ++dp2[v];
+    forup(int, i, 0, 19) forup(int, mask, 0, (1 << 20) - 1) if (getBit(mask, i))
+    {
+        dp1[mask] += dp1[mask ^ (1 << i)];
+        dp2[mask ^ (1 << i)] += dp2[mask];
+    }
+    for (int v : arr)
+        cout << dp1[v] << ' ' << dp2[v] << ' ' << n - dp1[((1 << 20) - 1) ^ v] << endl;
+    return 0;
+}
